f085e2798412782ab3aa4811cff5d329b05a705a
satish_mdzpp <157603741+steevamindzapp@users.noreply.github.com>
2025-07-01 19:52:25 +0530

    Refactor controllers for async/await and improve validation
    Refactored multiple controllers to use async/await for database operations, improving error handling and code clarity. Enhanced input validation and error logging in auth, category, item, and stock controllers. Updated the route for fetching all users from '/allUsers' to '/users'. Improved transaction handling for item deletion and stock out operations.

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -8,7 +8,7 @@ authRouter.post("/send-otp", authController.sendOtp);
 authRouter.post("/otpverify", authController.verifyOtp);
 authRouter.post("/update-password", authController.updatePassword);
 authRouter.post('/email-verification',authController.emailverify);
-authRouter.get('/allUsers',authController.getAllUsers);
+authRouter.get('/users',authController.getAllUsers);
 authRouter.put('/update-permit/:userId/:permit', authController.updateUserPermission);
 authRouter.put('/update-plan-date/:userId', authController.updatePlanEndDate);
 module.exports = authRouter;
